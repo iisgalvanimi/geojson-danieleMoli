2ce3bd2e0fc4073c578c6724d66cbb9d48509491
danieleMoli <molinari.daniele@iisgalvanimi.edu.it>
2020-04-08 13:13:13 +0000

    esercizio

---
+++ MolinariApp/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AgmCoreModule.forRoot({apiKey: 'AIzaSyCnG7Tsbe6-oK4XGcnOxCpuGl1QS50XltM'}),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+
+}
+
